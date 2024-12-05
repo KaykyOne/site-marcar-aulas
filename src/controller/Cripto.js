d20263b3afbd8a13b45bf2d41cdff516c229f561
kayky <kaykyzioti@gmail.com>
2024-12-05 14:26:03 -0300

    Updates

--- src/controller/Cripto.js
+++
@@ -1,9 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-const Cripto = (input) => {
-    const combinedInput = input + process.env.REACT_APP_SECRET_KEY; // Use REACT_APP_ para variáveis de ambiente
-    const hash = CryptoJS.SHA256(combinedInput).toString(CryptoJS.enc.Hex);
-    return hash;
-};
-
-export default Cripto;
